5bf00dbadfccff50c92fda8bb618d4799ee12b8f
0xViper <the0xviper@gmail.com>
2021-03-16 10:33:07 +0100

    Add bsc token lists

--- src/buildList.js
+++ src/buildList.js
@@ -1,11 +1,13 @@
 const { version } = require("../package.json");
-const mainnet = require("./tokens/mainnet.json");
+/*const mainnet = require("./tokens/mainnet.json");
 const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const harmony_mainnet = require("./tokens/harmony-mainnet.json");
-const harmony_testnet = require("./tokens/harmony-testnet.json");
+const kovan = require("./tokens/kovan.json");*/
+const bscMainnet = require("./tokens/bsc-mainnet.json");
+const bscTestnet = require("./tokens/bsc-testnet.json");
+const harmonyMainnet = require("./tokens/harmony-mainnet.json");
+const harmonyTestnet = require("./tokens/harmony-testnet.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -20,7 +22,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://dvwecb5klcqus.cloudfront.net/venomswap/logos/venomswap-128x128.png",
     keywords: ["venomswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...harmony_mainnet, ...harmony_testnet]
+    tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
